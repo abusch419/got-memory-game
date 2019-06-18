865c6de660848a30160e45207b122231cd8658bb
Abusch419 <abusch419@gmail.com>
2019-06-18 12:43:36 -0400

    Updates

---
+++ precache-manifest.1286df492189fc66583a4fcc9edc5a44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2f36e3ff9b2be5f6437096686185ff",
+    "url": "/got-memory-game/index.html"
+  },
+  {
+    "revision": "3726035ec057a06111ce",
+    "url": "/got-memory-game/static/css/main.edffa4f9.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/got-memory-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "3726035ec057a06111ce",
+    "url": "/got-memory-game/static/js/main.907e16c9.chunk.js"
+  },
+  {
+    "revision": "5c0c8ac70e6939f874bc",
+    "url": "/got-memory-game/static/js/runtime~main.f4c69331.js"
+  }
+]);
